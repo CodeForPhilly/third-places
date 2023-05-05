f3308757ee584acd1a4f41b3c743cbc69b21b868
Michael Carpenter <mcarpenter.dev@gmail.com>
2023-05-04 22:10:03 -0400

    Add initial user endpoints

--- src/django/api/urls.py
+++ src/django/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("users/", views.third_place_user_list),
+    path("users/<int:pk>", views.third_place_user_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- src/django/api/views.py
+++ src/django/api/views.py
@@ -1,5 +1,45 @@
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ThirdPlaceUserSerializer, ThirdPlaceUserPutSerializer
+from .models import ThirdPlaceUser
 
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+@api_view(['GET', 'POST'])
+def third_place_user_list(request):
+    if request.method == 'GET':
+        users = ThirdPlaceUser.objects.all()
+        serializer = ThirdPlaceUserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ThirdPlaceUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+def third_place_user_detail(request, pk):
+    try:
+        user = ThirdPlaceUser.objects.get(pk=pk)
+    except ThirdPlaceUser.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ThirdPlaceUserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ThirdPlaceUserPutSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
